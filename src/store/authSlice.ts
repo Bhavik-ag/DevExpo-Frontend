5592344f465986c58257db8241037c183f760243
Bhavik Agarwal <bhavikiitian@gmail.com>
2023-06-17 13:58:10 +0530

    chore: intial setup

---
+++ src/store/authSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+export interface AuthState {
+  user: string | null;
+  token: string | null;
+}
+
+const initialState: AuthState = {
+  user: null,
+  token: null,
+};
+
+export interface AuthPayloadType {
+  user: string;
+  accessToken: string;
+}
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {
+    setCredentials: (state, action: PayloadAction<AuthPayloadType>) => {
+      const { user, accessToken } = action.payload;
+      state.user = user;
+      state.token = accessToken;
+    },
+    logOut: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { setCredentials, logOut } = authSlice.actions;
+export default authSlice.reducer;
